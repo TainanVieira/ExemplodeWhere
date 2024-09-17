6e152338c8617b83e7ebaa0e37e8bad10b72ec4a
f-Brizola <brizola95@gmail.com>
2024-09-16 22:28:27 -0400

    tabuada

---
+++ src/ContagemRegressiva.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class ContagemRegressiva {
+    public static void main(String[] args) {
+
+        int valor = 0;
+
+        Scanner ler = new Scanner(System.in);
+        System.out.println("Digite um numero ");
+        valor = ler.nextInt();
+        while (valor > 0) {
+            valor= valor-1;
+            System.out.println(valor);
+
+        }
+
+
+
+
+
+
+
+    }
+}
